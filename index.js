c4b0aa13ec69ea477abda4c333369dfa720fd2bc
ActiveShayun <wevdeveloper0477gmail.com>
2025-03-10 18:54:10 +0600

    add planet api done

---
+++ index.js
@@ -0,0 +1,123 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId, Timestamp } = require('mongodb')
+const jwt = require('jsonwebtoken')
+const morgan = require('morgan')
+
+const port = process.env.PORT || 9000
+const app = express()
+// middleware
+const corsOptions = {
+  origin: [ 'http://localhost:5173'
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(morgan('dev'))
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token
+
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err)
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.user = decoded
+    next()
+  })
+}
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xlnwpku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+async function run() {
+  try {
+    const usersCollection = client.db('planet').collection('users')
+    const plantCollection = client.db('planet').collection('planets')
+
+    // Generate jwt token
+    app.post('/jwt', async (req, res) => {
+      const email = req.body
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '365d',
+      })
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true })
+    })
+    // Logout
+    app.post('/logout', async (req, res) => {
+      try {
+        res
+          .clearCookie('token', {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          })
+          .send({ success: true })
+      } catch (err) {
+        res.status(500).send(err)
+      }
+    })
+
+    // users collection
+    app.post('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = req.body;
+      console.log(user);
+      const userExits = await usersCollection.findOne(query);
+      if (userExits) {
+        return res.send({ message: 'user all ready exits' })
+      }
+
+      const result = await usersCollection.insertOne({ ...user, timestamp: Date.now() });
+      res.send({ result })
+    })
+
+    app.post('/add-planet', async (req, res) => {
+      const planets = req.body;
+      console.log('planets', planets);
+      const result = await plantCollection.insertOne(planets)
+      res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 })
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+  res.send('Hello from plantNet Server..')
+})
+
+app.listen(port, () => {
+  console.log(`plantNet is running on port ${port}`)
+})
